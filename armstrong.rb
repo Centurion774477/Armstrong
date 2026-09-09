187f29cd337694ea87e33986f9995d66c709bb0d
Reece Turner <reeceturner1358@gmail.com>
2026-09-09 17:34:10 -0400

    fixed

--- armstrong.rb
+++ armstrong.rb
@@ -1,16 +1,12 @@
 
 class Armstrong
-  def given line, regex, call
+  def self.given line, regex, call:
 
     match = line.match regex
 
     return unless match
 
     capture_groups = regex.source.scan(/\?\<([a-zA-Z0-9_]+)\>/).flatten
-
-    unless call.respond_to?
-      fail "There is no function to answer this call: #{call}"
-    end
 
     if capture_groups.empty?
       send(call)
